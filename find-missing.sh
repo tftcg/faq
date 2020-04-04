da3c1ee2a43bc726bc4b7efb570f340189431ab3
hen <dev@flamefew.com>
2020-04-04 12:34:14 -0700

    Script to identify faq entries not shown on the site

---
+++ find-missing.sh
@@ -0,0 +1,3 @@
+grep  ' id="' faqxml/* | sed 's/.xml: *<entry id="/__/' | sed 's/".*//' | sed 's/faqxml\//#/' | sort -u > FAQ_IDS
+find docs/ -type f -name '*.html' | xargs grep 'faq_entry' | sed 's/^.* id="/#/' | sed 's/".*//' | sort -u > PUBLISHED_IDS
+diff FAQ_IDS PUBLISHED_IDS > DIFF_IDS

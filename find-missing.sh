d8cfdcb613b932c6a0477a0bd4bdbb7fcf1279bd
hen <dev@flamefew.com>
2020-04-04 13:39:16 -0700

    Ignore xref entries

--- find-missing.sh
+++ find-missing.sh
@@ -1,3 +1,3 @@
-grep  ' id="' faqxml/* | sed 's/.xml: *<entry id="/__/' | sed 's/".*//' | sed 's/faqxml\//#/' | sort -u > FAQ_IDS
+grep  ' id="' faqxml/* | grep -v 'xref' | sed 's/.xml: *<entry id="/__/' | sed 's/".*//' | sed 's/faqxml\//#/' | sort -u > FAQ_IDS
 find docs/ -type f -name '*.html' | xargs grep 'faq_entry' | sed 's/^.* id="/#/' | sed 's/".*//' | sort -u > PUBLISHED_IDS
 diff FAQ_IDS PUBLISHED_IDS > DIFF_IDS

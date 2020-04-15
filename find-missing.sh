9663a0f542ee6ee8ba717630042d612e841abc67
hen <dev@flamefew.com>
2020-04-15 16:17:28 -0700

    Clean up

--- find-missing.sh
+++ find-missing.sh
@@ -1,3 +1,4 @@
 grep  ' id="' faqxml/* | grep -v 'xref' | sed 's/.xml: *<entry id="/__/' | sed 's/".*//' | sed 's/faqxml\//#/' | sort -u > FAQ_IDS
 find docs/ -type f -name '*.html' | xargs grep 'faq_entry' | sed 's/^.* id="/#/' | sed 's/".*//' | sort -u > PUBLISHED_IDS
 diff FAQ_IDS PUBLISHED_IDS > DIFF_IDS
+rm FAQ_IDS PUBLISHED_IDS
